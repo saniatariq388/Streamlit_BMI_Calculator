214e5b0ab764685828fe8d7cffc573fa996958bb
saniatariq388 <saniatariq388@gmail.com>
2025-03-27 07:01:21 +0500

    up2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import random
+import time
 
 st.title("BMI Calculator")
 
@@ -8,7 +10,31 @@ weight = st.slider("Enter your weight  (in kg):", 40, 200, 70)
 
 bmi = weight / ((height/100) ** 2)
 
+
+# # Show confetti effect if the BMI is calculated
+if bmi:
+#     st.balloons()  # Display the party popper effect (confetti)
+    st.snow()      # Snowfall effect (optional) 
+    with st.spinner('Calculating BMI...'):
+       time.sleep(2)  # Simulate some delay (e.g., complex calculations)
+    st.success('Done!')
+
+
+
 st.write(f"Your BMI is {bmi:.2f}")
+
+# Provide suggestion based on BMI categories
+if bmi < 18.5:
+    st.write("### Suggestion: You are underweight. It's important to maintain a healthy weight for your well-being.")
+elif 18.5 <= bmi < 24.9:
+    st.write("### Suggestion: Your weight is normal. Keep up the good work!")
+elif 25 <= bmi < 29.9:
+    st.write("### Suggestion: You are overweight. Consider adopting a healthier diet and exercise routine.")
+else:
+    st.write("### Suggestion: You are in the obesity category. It's important to seek guidance from a healthcare provider for a personalized health plan.")
+
+
+
 
 st.write("### BMI Categories ###")
 st.write("- Underweight: BMI less than 18.5")
